d7edcad4e791305123f5fbdee8506464cf813f63
Roman Grinyov <w3lifer@gmail.com>
2023-11-27 13:59:47 +0300

    added `JSON_PRETTY_PRINT`

--- .docker/.helpers/configure-project.php
+++ .docker/.helpers/configure-project.php
@@ -22,4 +22,4 @@ file_put_contents(PATH_TO_ENV, $config);
 
 $packageJsonContent = json_decode(file_get_contents('package.json'), true);
 $packageJsonContent['scripts']['dev'] = 'vite --host --port=${VITE_PORT}';
-file_put_contents('package.json', $packageJsonContent);
+file_put_contents('package.json', json_encode($packageJsonContent, JSON_PRETTY_PRINT));
