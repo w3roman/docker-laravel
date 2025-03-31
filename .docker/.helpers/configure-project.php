8790676131b3a078e7462d83354265ab31172949
Roman Grinyov <w3lifer@gmail.com>
2025-04-01 01:49:27 +0300

    use envars in app/.env

--- .docker/.helpers/configure-project.php
+++ .docker/.helpers/configure-project.php
@@ -8,12 +8,12 @@ $config = file_get_contents(PATH_TO_ENV);
 
 $config = preg_replace('=APP_URL\=http://localhost=', 'APP_URL=http://localhost:${NGINX_PORT}', $config);
 
-$config = preg_replace('=DB_CONNECTION\=sqlite=', 'DB_CONNECTION=' . getenv('DB_CONNECTION'), $config);
-$config = preg_replace('=# DB_HOST\=127\.0\.0\.1=', 'DB_HOST=' . getenv('DB_HOST'), $config);
-$config = preg_replace('=# DB_PORT\=3306=', 'DB_PORT=' . getenv('DB_PORT'), $config);
-$config = preg_replace('=# DB_DATABASE\=laravel=', 'DB_DATABASE=' . getenv('DB_DATABASE'), $config);
-$config = preg_replace('=# DB_USERNAME\=root=', 'DB_USERNAME=' . getenv('DB_USERNAME'), $config);
-$config = preg_replace('=# DB_PASSWORD\==', 'DB_PASSWORD=' . getenv('DB_PASSWORD'), $config);
+$config = preg_replace('=DB_CONNECTION\=sqlite=', 'DB_CONNECTION=${DB_CONNECTION}', $config);
+$config = preg_replace('=# DB_HOST\=127\.0\.0\.1=', 'DB_HOST=${DB_HOST}', $config);
+$config = preg_replace('=# DB_PORT\=3306=', 'DB_PORT=${DB_PORT}', $config);
+$config = preg_replace('=# DB_DATABASE\=laravel=', 'DB_DATABASE=${DB_DATABASE}', $config);
+$config = preg_replace('=# DB_USERNAME\=root=', 'DB_USERNAME=${DB_USERNAME}', $config);
+$config = preg_replace('=# DB_PASSWORD\==', 'DB_PASSWORD=${DB_PASSWORD}', $config);
 
 file_put_contents(PATH_TO_ENV, $config);
 
