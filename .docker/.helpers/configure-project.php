0a97e8cbfb2daea23a6390e4fe2e8184be4a7650
Roman Grinyov <w3lifer@gmail.com>
2025-04-12 03:03:29 +0300

    HTTPS: initial

--- .docker/.helpers/configure-project.php
+++ .docker/.helpers/configure-project.php
@@ -6,7 +6,7 @@ const PATH_TO_ENV = '.env';
 
 $config = file_get_contents(PATH_TO_ENV);
 
-$config = preg_replace('=APP_URL\=http://localhost=', 'APP_URL=http://localhost:${NGINX_PORT}', $config);
+$config = preg_replace('=APP_URL\=http://localhost=', 'APP_URL=https://localhost:${_NGINX_PORT_HTTPS}', $config);
 
 $config = preg_replace('=DB_CONNECTION\=sqlite=', 'DB_CONNECTION=${DB_CONNECTION}', $config);
 $config = preg_replace('=# DB_HOST\=127\.0\.0\.1=', 'DB_HOST=${DB_HOST}', $config);
@@ -22,3 +22,22 @@ file_put_contents(PATH_TO_ENV, $config);
 $packageJsonContent = json_decode(file_get_contents('package.json'), true);
 $packageJsonContent['scripts']['dev'] = 'vite --host --port=${VITE_PORT}';
 file_put_contents('package.json', json_encode($packageJsonContent, JSON_PRETTY_PRINT));
+
+// vite.config.js
+
+$viteConfigJsContent = file_get_contents('vite.config.js');
+$viteConfigJsContent = preg_replace(
+    '=export default defineConfig\(\{=',
+    '$1' . <<<SERVER
+
+    server: {
+        https: {
+            cert: '/etc/nginx/certs/localhost-cert.pem',
+            key: '/etc/nginx/certs/localhost-key.pem',
+        },
+    },
+SERVER
+,
+    $viteConfigJsContent
+);
+file_put_contents('vite.config.js', $viteConfigJsContent);
